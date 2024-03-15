8bad595cc0c11c2f0fc74d60c674f2a9e098b0ef
Eduardo Calvo <calvouribe.eduardo@gmail.com>
2024-03-15 11:26:53 +0100

    Made FakeRequest Async

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import RowBox from './components/RowBox.vue'
 
-// Fake REST API request!!
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json()) // takes response, converts to JavaScript Promise object
-  .then(json => {
+getFakeRequest();
+
+async function getFakeRequest() {
+  try {
+    // Fake API fetch
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1') 
+
+    // takes response, converts to JavaScript Promise object
+    const json = await response.json() 
 
     // Selects first div with 'fake_request' id attribute
     const fakeDiv = document.getElementById('fake_request');
@@ -13,8 +18,10 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
     // For full JSON string, use: '<pre>' + JSON.stringify(json, null, 2) + '</pre>'
     fakeDiv.innerHTML = '<p><h4>Fake Request Data:</h4><br>' + json.title + json.body + '</p>';
 
-  }) 
-  .catch(error => console.error('Error:', error)); // Error catching 
+  } catch (error) {
+    console.error('Error:', error)
+  }   
+}
 
 </script>
 
