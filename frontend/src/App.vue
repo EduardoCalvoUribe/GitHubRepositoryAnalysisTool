3214dccffd2990c6f7f217d8e98343c494f3661b
Eduardo Calvo <calvouribe.eduardo@gmail.com>
2024-03-15 12:52:12 +0100

    BackendToFrontend JSON working

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,18 +5,20 @@ getFakeRequest();
 
 async function getFakeRequest() {
   try {
+
     // Fake API fetch
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1') 
+    const response = await fetch('http://127.0.0.1:8000/github/user')  //'https://jsonplaceholder.typicode.com/posts/1'  //'127.0.0.1:8000/github/user'
 
     // takes response, converts to JavaScript Promise object
     const json = await response.json() 
 
     // Selects first div with 'fake_request' id attribute
     const fakeDiv = document.getElementById('fake_request');
-
+    
     // Inserts content into that div in the specified format
     // For full JSON string, use: '<pre>' + JSON.stringify(json, null, 2) + '</pre>'
-    fakeDiv.innerHTML = '<p><h4>Fake Request Data:</h4><br>' + json.title + json.body + '</p>';
+    fakeDiv.innerHTML = '<pre>' + JSON.stringify(json, null, 2) + '</pre>'
+    // fakeDiv.innerHTML = '<p><h4>Fake Request Data:</h4><br>' + json.url + '</p>';
 
   } catch (error) {
     console.error('Error:', error)
