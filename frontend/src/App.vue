7b1fe1e3c731fb32f5c11a6fe78fd36332080185
Eduardo Calvo <calvouribe.eduardo@gmail.com>
2024-04-18 14:27:18 +0200

    loop over fake tracked repo json data and display

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { fetchData } from './fetchData.js'
 
 import Badge from './components/Badge.vue';
@@ -17,18 +17,59 @@ export default {
     BaseInput
   },
   setup() {
+    const repoTable = ref('');
+
+    async function getTrackedRepositories() {
+      try {
+        console.log('getTrackedRepositories called') 
+        const data = [
+            {
+                "name": "repo1",
+                "last_accessed": "2022-01-01"
+            },
+            {
+                "name": "repo2",
+                "last_accessed": "2022-01-02"
+            },
+            {
+                "name": "repo3",
+                "last_accessed": "2022-01-03"
+            },
+            {
+                "name": "repo4",
+                "last_accessed": "2022-01-04"
+            }
+        ];
+
+        //const data = await response.json();
+        let table = '';
+        for (let i = 0; i < data.length; i++) {
+          table += '<p><h5>Repository ' + i.toString() + ':</h5><br>' + JSON.stringify(data[i]) + '</p><br>';
+        }
+        repoTable.value = table;
+      } catch (error) {
+        console.error('Error:', error);
+        repoTable.value = "Error: Could not fetch tracked repositories.";
+      } 
+    }
+
     onMounted(async () => {
       try {
+        await getTrackedRepositories();
         // Fetches json data from specified URL using our fetchData function (will be our backend endpoints)
         // const json_response = await fetchData('http://127.0.0.1:8000/github/user');
-        const fake_response = await fetchData('http://jsonplaceholder.typicode.com/posts/1');
+        // const fake_response = await fetchData('http://jsonplaceholder.typicode.com/posts/1');
 
         // Selects first div with specified id (such as 'github_request')
-        const fakeDiv = document.getElementById('fake_request');
+        // const fakeDiv = document.getElementById('fake_request');
 
         // Inserts content of json into that div in whatever specified format
-        fakeDiv.innerHTML = '<pre>' + JSON.stringify(fake_response, null, 2) + '</pre>';
+        // fakeDiv.innerHTML = '<pre>' + JSON.stringify(fake_response, null, 2) + '</pre>';
+        
 
+        // this.tableContent = '<p>Test content for tableContent</p>';
+        // this.githubResponse = '<p>Test content for githubResponse</p>';
+        // getTrackedRepositories();
       } catch (error) {
         console.error('Error:', error)
       }
@@ -38,17 +79,22 @@ export default {
       // return { githubURL };
 
     });
+
+    return { repoTable };
   },
   // TODO: Fix githubURL, it's just empty. Input is not correctly retrieved from Input Box.
   data() {
     return {
-      githubURL: ''
+      githubURL: '',
+      tableContent: '',
+      githubResponse: ''
     }
   },
   methods: {
     async handleGithubURLSubmit() {
       //var githubURL = document.getElementById('githubURL').value;
-      console.log(githubURL)
+      console.log('handleGithubURLSubmit called') 
+      console.log(this.githubURL)
       const data = {'url': this.githubURL};
 
       const postOptions = {
@@ -65,14 +111,13 @@ export default {
           const response = await fetchData('http://127.0.0.1:8000/github/github-pulls/'.concat(githubURL), postOptions);
           // const response = await fetch(url, postOptions);
           // const json = await response.json();
-          const githubDiv = document.getElementById('github_request');
-          githubDiv.innerHTML = '<p><h5>Data from Backend:</h5><br>' + JSON.stringify(response) + '</p>';
+          this.githubResponse = '<p><h5>Data from Backend:</h5><br>' + JSON.stringify(response) + '</p>';
 
       } catch (error) {
           console.error('Error:', error);
       }
     },
-  },  
+  }
 };
 </script>
 
@@ -92,7 +137,14 @@ export default {
       </div>
     </div>
 
-    <div style="margin-top: 10%;" id="github_request"></div>
+    <div v-html="repoTable"></div>
+
+    <div v-html="tableContent"></div>
+
+    <!-- id="github_request" -->
+    <div style="margin-top: 10%;" v-html="githubResponse" ></div> 
+
+
     <!-- <br><br>
     <h5>Fake Request:</h5>
     <div id="fake_request"></div>
