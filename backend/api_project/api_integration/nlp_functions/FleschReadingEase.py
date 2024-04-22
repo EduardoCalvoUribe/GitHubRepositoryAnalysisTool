d61a7e55fd6104bd5ddffbe3068d7341a268c362
danilkaru <93515400+danilkaru@users.noreply.github.com>
2024-04-22 17:48:25 +0200

    flesch reading ease bug fixes/new additions

--- backend/api_project/api_integration/nlp_functions/FleschReadingEase.py
+++ backend/api_project/api_integration/nlp_functions/FleschReadingEase.py
@@ -1,11 +1,9 @@
+# TO ADD (minor bug solving): This function does not yet properly handle URLs, for instance the string "github.com" currently gets 
+# a low Flesch reading ease score way below 0. Furthermore, the output for standalone words (so a string containing only 1 word)
+# which are unknown to cmudict get Flesch reading ease scores which vary highly, often being either 36, 121 or way below 0. 
+# Furthermore, decide if negative Flesch reading ease scores should be set as 0 to have clearly defined bounds for semantic score.
 
-# This file contains the classes which are used for calculating NLP metrics on commit messages in our webapp.
-
-# For testing purposes, the classes are also available in the Google Drive project folder. (Remove this comment line for end product)
-
-
-# from django.http import HttpResponse
-
+# NOTE: for this function, the nltk dependency must be installed (pip install nltk)
 import nltk
 
 # download cmudict
@@ -21,28 +19,45 @@ from nltk.tokenize import sent_tokenize
 
 
 #Function which calculates the Flesch reading ease metric for a given message.
+#Returns Flesch reading ease as a single numerical value
 def calculate_flesch_reading_ease(message):
-    # Word count equals message string length
-    wordCount = len(message)
+    # Count number of words with .split() function. By default the separator character is any whitespace
+    wordCount = len(message.split())
     # Count sentences using nltk sent_tokenize
     sentenceCount = len(sent_tokenize(message))
     # Initialise syllable count for message using syllable_count function.
     syllableCount = 0
 
     # Count total syllable count
-    for word in message:
+    for word in message.split():
         syllableCount += syllable_count(word)
     
     # Calculate Flesch reading ease score with the associated formula
-    flesch_reading_ease_score = 206.835 - (1.015 * (wordCount/sentenceCount)) - (84.6 * (syllableCount/wordCount))
+    # Initialise Flesch reading ease score as 0 
+    flesch_reading_ease_score = 0
+
+    # If wordCount or sentenceCount are 0, let Flesch reading ease score be 
+    # out of bounds (max Flesch reading ease score is around 121) due to undefined division by 0
+    if (sentenceCount == 0 or wordCount == 0):
+        flesch_reading_ease_score = 1000
+    else:
+    # else calculate Flesch reading ease score
+        flesch_reading_ease_score = 206.835 - (1.015 * (wordCount/sentenceCount)) - (84.6 * (syllableCount/wordCount))
 
     # Return Flesch reading ease score.
-    # If Flesch reading ease score is above 0, return this value.
-    if flesch_reading_ease_score > 0:
-        return str(flesch_reading_ease_score)
-    # Else if Flesch reading ease score is below 0 or 0, return 0.
-    else:
-        return str(0)
+    # If Flesch reading ease score is 1000, then an undefined division by 0 has occurred. 
+    return flesch_reading_ease_score 
+
+
+    # Code for future reference, might be relevant if lower bound for Flesch reading ease score is set to 0.
+    # if flesch_reading_ease_score > 0:
+    #     return flesch_reading_ease_score
+    # # Else if Flesch reading ease score is 0, return 0.
+    # elif flesch_reading_ease_score == 0:
+    #     return 0
+    # # Return -1 if sentenceCount = 0 or wordCount = 0 (undefined division by 0 in formula)
+    # elif flesch_reading_ease_score < 0:
+    #     return -1
 
 #Helper function which checks if word is in cmudict dictionary.
 def lookup_word(word):
@@ -64,7 +79,6 @@ def syllable_count(word):
 
 # Function with simple heuristic for determining the number of syllables in English words. 
 # All words which are not in cmudict are processed by this function. 
-
 # Courtesy of https://datascience.stackexchange.com/questions/23376/how-to-get-the-number-of-syllables-in-a-word, 
 # slightly adjusted and improved by ChatGPT 3.5. 
 def otherSyllables(word):
@@ -96,11 +110,9 @@ def otherSyllables(word):
     # Avoid counting 0 syllables
     return max(count, 1)
 
-# This function computes the average flesch reading ease for a list of message strings.
-# messageList must be a list of strings.
-
+# This function computes the average flesch reading ease for a list of message strings. messageList must be a list of strings.
 # NOTE: The average flesch reading ease score can also be calculated with aggregate avg() function in sqlite database. 
-# However, this is only possible if we decide to stoe the flesch reading ease scores in the database. 
+# However, this is only possible if we decide to store the flesch reading ease scores in the database. 
 def calculate_average_flesch_reading_ease(messageList):
   # Initialise total flesch reading ease variable
   total_flesch_reading_ease = 0
