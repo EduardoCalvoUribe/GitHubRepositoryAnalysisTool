835ac8f571f6d67450149b4189b7b83276d24a11
Marie <marie.oldeman@ru.nl>
2024-04-25 12:48:40 +0200

    The router files from a template file added to the project in a seperate folder.

---
+++ router/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+  <header>
+    
+    <div class="wrapper">
+      <HelloWorld msg="Repository Analysis Tool" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/RepositoryAnalysis">Repository Analysis</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
