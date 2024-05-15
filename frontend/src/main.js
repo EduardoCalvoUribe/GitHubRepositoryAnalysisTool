3787a2f81b1e20af602967e09f11dfe23040be2d
Eduardo Calvo <calvouribe.eduardo@gmail.com>
2024-05-15 09:30:35 +0200

    delete extra frontend folder

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import Argon from '@/plugins/argon-kit'
-
-const vue_app = createApp(App)
-vue_app.use(Argon);
-vue_app.mount('#app')
