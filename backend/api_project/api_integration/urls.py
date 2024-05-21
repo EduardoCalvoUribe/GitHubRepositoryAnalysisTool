3a7ec1719e588e6c1e5f2acdee073de4a00873fd
danilkaru <93515400+danilkaru@users.noreply.github.com>
2024-05-21 02:23:45 +0200

    added 2 url paths for semantic score testing

--- backend/api_project/api_integration/urls.py
+++ backend/api_project/api_integration/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views, comment_info, API_call_information, functions
+from . import views, comment_info, API_call_information,general_semantic_score
 
 
 # importing nlp_functions to test function output on webpage. 
@@ -20,4 +21,7 @@ urlpatterns = [
     path('all/', API_call_information.get_github_information, name = 'all'),
     path('database/', views.frontendInfo, name = 'database'),
     path('delete/', views.delete_entry_db, name = 'delete')
+    path('all/', API_call_information.get_github_information, name = 'all'), 
+    path('semantic',general_semantic_score.displaySemantic, name = 'semantic'),
+    path('commentList',general_semantic_score.get_comments, name = 'commentList'),
 ]
