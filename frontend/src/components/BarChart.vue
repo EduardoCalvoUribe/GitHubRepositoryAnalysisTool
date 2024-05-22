b25943c8e95aeeb36244aa2979047d2f19cec0cf
Eduardo Calvo <calvouribe.eduardo@gmail.com>
2024-05-22 11:24:44 +0200

    basic bar chart

---
+++ frontend/src/components/BarChart.vue
@@ -0,0 +1,32 @@
+<template>
+    <Bar
+      :data="chartData"
+      :options="chartOptions"
+    />
+  </template>
+  
+  <script>
+  import { Bar } from 'vue-chartjs'
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+  
+  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+  
+  export default {
+    name: 'BarChart',
+    components: { Bar },
+    data() {
+      return {
+        chartData: {
+          labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [ { data: [0.8, 0.2, 0.3, 0.6, 0.55, 0.23, 0.95] } ],
+          type: Object,
+          required: true
+        },
+        chartOptions: {
+          type: Object,
+          default: () => {}
+        }
+      }
+    }
+  }
+  </script>
