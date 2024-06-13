488bed0d8863a02306fdb5973de6d68d9e653c32
Eduardo Calvo <calvouribe.eduardo@gmail.com>
2024-06-13 19:51:27 +0200

    temporarily disable login feature

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -44,6 +44,10 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
+  console.log("Login temporarily disabled. Check index.js to enable.")
+  next();
+  return;
+  
   const authToken = localStorage.getItem("authToken");
   const isLoggedIn = !!authToken;
 
